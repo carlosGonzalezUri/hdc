417eeac5a2bbf34ab484a1dc035abba2e06a7eca
Carlos Gonzalez Uri <carlos@MacBook-Air-de-Carlos.local>
2018-07-12 20:49:46 +0200

    acabado anuales - empezando fiestas

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+import { Fiestas } from '../fiestas/fiestas';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { Anuales } from '../anuales/anuales';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab1Root = Anuales;
+  tab2Root = Fiestas;
   tab3Root = ContactPage;
 
   constructor() {
